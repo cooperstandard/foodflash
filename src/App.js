b7a5360f4fb706b758739430fd1cd70f4c4278a9
Cooper <cooper@cooperstandard.org>
2023-01-30 17:23:39 -0700

    cors

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios';
+import React,{useEffect, useState} from 'react';
 
 function App() {
+
+  const [recipes, setRecipes]=useState([])
+/*
+  const fetchData = () => {
+    fetch("http://concierge.cooperstandard.org:3000/api/all")
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        console.log(data)
+        setRecipes(data)
+      })
+  }
+*/
+  useEffect(() => {
+    fetchRecipes();
+  }, [])
+
+  /*
+  useEffect(() => {
+    console.log(recipes)
+  }, [recipes]);
+  */
+  const fetchRecipes = async() => {
+    const response = Axios("/api/all");
+    console.log(response)
+    setRecipes(response.data);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        recipes && recipes.map(recipe => {
+
+          return(
+            <div key={recipe.id} style={{alignItems:'center',margin:'20px 60px'}}>
+            <h4>{recipe.title}</h4>
+            
+
+            </div>
+          )
+
+
+        })
+      }
+    
     </div>
   );
 }
