d4f5398206c44533cec860698684938eb1c067b5
BasedWRLD <kevin2845ca@gmail.com>
2023-02-20 17:57:35 -0700

    added account page

---
+++ src/Account.js
@@ -0,0 +1,13 @@
+import './Recipes.css';
+import './App.css'
+import Axios from 'axios';
+import React, { useState, useEffect, useLayoutEffect, useRef } from 'react';
+import { useParams, useLocation, useNavigate, useNavigation } from 'react-router-dom';
+
+function Account(){
+
+
+
+}
+
+export default Account;
